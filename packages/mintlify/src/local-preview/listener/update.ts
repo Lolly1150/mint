cb082dbdd1883315ecf4943b983266862de51a4c
Hahnbee Lee <55263191+hahnbeelee@users.noreply.github.com>
2023-02-07 23:14:26 -0800

    delete unused import

--- packages/mintlify/src/local-preview/listener/update.ts
+++ packages/mintlify/src/local-preview/listener/update.ts
@@ -1,12 +1,9 @@
 // TODO: Add Types
 import fse from "fs-extra";
-import { promises as _promises } from "fs";
 import { CLIENT_PATH, CMD_EXEC_PATH } from "../../constants.js";
 import { join } from "path";
 import { generateNav } from "./generate.js";
 import { categorizeFiles } from "./categorize.js";
-
-const { readFile } = _promises;
 
 // TODO: Put in prebuild package
 
